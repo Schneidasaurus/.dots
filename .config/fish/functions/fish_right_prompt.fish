236838c95704ba750ebc8c9080251bc167d4f07c
Andrew Schneider <andrewkschneider@pm.me>
2026-03-28 20:00:14 -0500

    improve fish_right_prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,30 @@
 function fish_right_prompt
 	if not contains -- --final-rendering $argv
-		string join '' -- \uf017 ' ' (math --scale 3 $CMD_DURATION / 1000) 's'
+		set -l remaining $CMD_DURATION
+		set -l duration_string ''
+		
+		set -l sec_ms 1000
+		set -l min_ms (math 60 x $sec_ms)
+		set -l hour_ms (math 60 x $min_ms)
+
+		set -l hours (math --scale 0 $remaining / $hour_ms)
+		if test $hours -gt 0
+			set duration_string (string join '' -- $duration_string $hours 'h')
+			set remaining (math $remaining % $hour_ms)
+		end
+
+		set -l minutes (math --scale 0 $remaining / $min_ms)
+		if test $minutes -gt 0
+			set duration_string (string join '' -- $duration_string $minutes 'm')
+			set remaining (math $remaining % $min_ms)
+		end
+
+		set -l seconds (math --scale 0 $remaining / $sec_ms)
+		if test $seconds -gt 0
+			set duration_string (string join '' -- $duration_string $seconds 's')
+			set remaining (math $remaining % $sec_ms)
+		end
+		set duration_string (string join '' -- $duration_string $remaining 'ms')
+		string join '' -- \uf017 ' ' $duration_string
 	end
 end
