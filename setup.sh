82a86ac2783018c9e6979b7e63d855b4fe74233c
Andrew Schneider <andrewkschneider@pm.me>
2026-03-30 21:24:14 -0500

    ansible improvements

--- setup.sh
+++ setup.sh
@@ -2,19 +2,22 @@
 
 DOTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-yay -S git
+yay --noconfirm -S --needed ansible
 
-ln -s $DOTS_DIR/tmux.conf $HOME/tmux.conf
-if ! [ -d $HOME/tmux ] ; then
-	mkdir $HOME/.tmux
-	git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-fi
-
-for config_dir in ls $DOTS_DIR/.config ; do
-	link_target="$DOTS_DIR/.config/$config_dir"
-	link_name="$HOME/.config/$config_dir"
-	if [ -d $link_name ]; then
-		rm -rf $link_name
-	fi
-	ln -s $link_target $link_name
-done
+ansible-playbook $DOTS_DIR/ansible/local.yml
+# yay -S git
+#
+# ln -s $DOTS_DIR/tmux.conf $HOME/tmux.conf
+# if ! [ -d $HOME/tmux ] ; then
+# 	mkdir $HOME/.tmux
+# 	git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+# fi
+#
+# for config_dir in ls $DOTS_DIR/.config ; do
+# 	link_target="$DOTS_DIR/.config/$config_dir"
+# 	link_name="$HOME/.config/$config_dir"
+# 	if [ -d $link_name ]; then
+# 		rm -rf $link_name
+# 	fi
+# 	ln -s $link_target $link_name
+# done
