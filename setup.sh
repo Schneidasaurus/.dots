614d023bf38122c3943546d14e900bc37a647049
Andrew Schneider <andrewkschneider@pm.me>
2026-04-04 09:13:37 -0500

    add flag to setup.sh to toggle dry-run

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+dry_run_args='-CD'
+for i in "$@"; do
+	case $i in
+		--frfr)
+			dry_run_args=''
+			shift 
+			;;
+	esac
+done
+
 DOTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-ansible-playbook $DOTS_DIR/ansible/local.yml --extra-vars "dots_dir=$DOTS_DIR" --ask-become-pass -CD
+ansible-playbook $DOTS_DIR/ansible/local.yml --extra-vars "dots_dir=$DOTS_DIR" --ask-become-pass ${dry_run_args}
 
